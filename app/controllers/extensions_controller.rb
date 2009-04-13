269887b4501340cb7702c1437b02a7cd99c44b4c
Juan José Vidal Agustín <juanjo@foundation.(none)>
2009-04-13 02:49:33 +0200

    Tag Cloud on footer

--- app/controllers/extensions_controller.rb
+++ app/controllers/extensions_controller.rb
@@ -69,6 +69,8 @@ class ExtensionsController < ApplicationController
     end
   end
 
+  
+
   private
   def load_data
     @available_versions = Version.all
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  include TagsHelper
   
   def flash_message
     messages = ""
@@ -22,6 +21,17 @@ module ApplicationHelper
         end
       end
       content_tag :ul, items, :id => 'tabnav'
+  end
+
+  def tag_cloud(tags, classes)
+    return if tags.empty?
+
+    max_count = tags.sort_by(&:count).last.count.to_f
+
+    tags.each do |tag|
+      index = ((tag.count / max_count) * (classes.size - 1)).round
+      yield tag, classes[index]
+    end
   end
 
 
